2985fdc6467e91733559d137c83efa175cd5417b
chandanswamy <chandanswamy13214@gmail.com>
2022-11-07 09:49:24 +0000

    API project complete

--- index.js
+++ index.js
@@ -148,3 +148,14 @@ app.delete("/books/:bookId", async (request, response) => {
   await db.run(deleteBookQuery);
   response.send("Book Deleted Successfully");
 });
+
+// GET Author related Books API
+
+app.get("/authors/:authorId/books", async (request, response) => {
+  const { authorId } = request.params;
+  const getAuthorBooksQuery = `
+    SELECT * FROM book WHERE author_id = ${authorId};`;
+
+  const booksArray = await db.all(getAuthorBooksQuery);
+  response.send(booksArray);
+});
